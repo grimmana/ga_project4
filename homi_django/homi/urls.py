019efe9fb615d14ef501329c3acd33777da45e8f
Ana Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-04-16 08:57:55 -0500

    create urls

---
+++ homi_django/homi/urls.py
@@ -0,0 +1,8 @@
+# homi/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.room_list, name='room_list'),
+    path('/items', views.item_list, name='item_list')
+]
--- homi_django/homi/views.py
+++ homi_django/homi/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Room, Item
 # Create your views here.
+
+# homi/views.py
+
+def room_list(request):
+    rooms = Room.objects.all()
+    return render(request, 'homi/room_list.html', {'rooms': rooms})
+
+def item_list(request):
+    items = Item_part.objects.all()
+    return render(request, 'homi/item_list.html', {'items': items})
