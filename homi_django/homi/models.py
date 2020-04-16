5983c52366fc9556cfc4890ff36dce8d17944e6e
Ana Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-04-16 03:09:20 -0500

    add models migrate create superuser seed  using admin

--- homi_django/homi/models.py
+++ homi_django/homi/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Item(models.Model):
+    category = models.CharField(max_length=100)
+    room_name = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='items')
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    serial_number = models.CharField(max_length=100)
+    purchase_date = models.CharField(max_length=100) 
+    purchase_price = models.CharField(max_length=100) 
+    warranty = models.CharField(max_length=100)
+    notes = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
