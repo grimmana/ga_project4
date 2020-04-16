09392345bc9660c66b4b0cabe6080724954f6a84
Ana Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-04-16 11:03:22 -0500

    can see webpages for localhost:8000 Rooms and localhost:8000/items/ Items

--- homi_django/homi/urls.py
+++ homi_django/homi/urls.py
@@ -6,6 +6,3 @@ urlpatterns = [
     path('', views.room_list, name='room_list'),
     path('items/', views.item_list, name='item_list')
 ]
-
-# WARNINGS:
-# ?: (urls.W002) Your URL pattern '/items' [name='item_list'] has a route beginning with a '/'. Remove this slash as it is unnecessary. If this pattern is targeted in an include(), ensure the include() pattern has a trailing '/'.
