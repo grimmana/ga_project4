027b49ce83db891de6d783fd72e0b0f0eec05a42
Ana Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-04-16 18:28:02 -0500

    updt urls.py with items/new

--- homi_django/homi/urls.py
+++ homi_django/homi/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('', views.room_list, name='room_list'),
     path('items/', views.item_list, name='item_list'),
     path('rooms/<int:pk>', views.room_detail, name='room_detail'),
-    path('items/<int:pk>', views.item_detail, name='item_detail')
-    path('rooms/new', views.rooms_create, name='room_create'),
+    path('items/<int:pk>', views.item_detail, name='item_detail'),
+    path('rooms/new', views.rooms_create, name='room_create'), 
+    path('items/new', views.items_create, name='item_create'), 
+
 
 ]
