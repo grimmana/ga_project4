dd16d2a371bfd0e2cc3bcddd0b5c00117ebea0fc
Ana Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-04-16 19:36:17 -0500

    urls.py add item_edit and item_detail.html h2 href

--- homi_django/homi/urls.py
+++ homi_django/homi/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('items/', views.item_list, name='item_list'),
     path('rooms/<int:pk>', views.room_detail, name='room_detail'),
     path('items/<int:pk>', views.item_detail, name='item_detail'),
-    path('rooms/new', views.rooms_create, name='room_create'), 
-    path('items/new', views.items_create, name='item_create'), 
+    path('rooms/new', views.room_create, name='room_create'), 
+    path('items/new', views.item_create, name='item_create'), 
     path('rooms/<int:pk>/edit', views.room_edit, name='room_edit'),
+    path('items/<int:pk>/edit', views.item_edit, name='item_edit'),
 
 
 ]
